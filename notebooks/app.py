49c6d7f2168aee01bc328fdf25434c25944630ef
Yesenia <yesy_enih.8@hotmail.com>
2024-06-28 16:23:35 -0500

    add notes file

--- notebooks/app.py
+++ notebooks/app.py
@@ -3,31 +3,29 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('notebooks\\vehicles_us.csv') # leer los datos
-
 st.header("Bienvenido(a) al Análisis de Vehiculos")
 
-hist_button = st.button('Histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Distancia recorrida por los vehiculos')
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Histograma')
+
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Distancia recorrida por los vehiculos')
             
             # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer")
         
             # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 
-disp_button = st.button('Gráfico de dispersión') # crear un botón
-        
-if disp_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Relación entre la Distancia recorrida del vehículo y su Precio actual')
+
+build_dispersion = st.checkbox('Gráfico de dispersión') 
+if build_dispersion:
+    st.write('Relación entre la Distancia recorrida del vehículo y su Precio actual')
             
-            # crear un histograma
-            fig =px.scatter(car_data, x="odometer", y="price")
+            # crear un grafico de dispersion
+    fig =px.scatter(car_data, x="odometer", y="price")
         
             # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
-
-
+    st.plotly_chart(fig, use_container_width=True)
